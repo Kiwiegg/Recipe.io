adae4ad9d73fa606a7f3140741284196994aae9b
Kiwiegg <larrywutoronto@gmail.com>
2021-01-16 14:07:24 -0500

    Restructuring

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors")l
 
 const PORT = 8888;
 
 var router = require("./routes/recipe.js");
 
 app.use("/api", router);
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("govind is gay");
