f36639a592f9fcc15e559d5f4f1470332f46231e
Kiwiegg <larrywutoronto@gmail.com>
2021-01-16 12:55:33 -0500

    More routing

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,1 +1,8 @@
-// JavaScript source code
+const express = require("express");
+const rounter = express.Router();
+
+router.get("/", function (req, res)) {
+    res.send("govind is gay");
+}
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -3,8 +3,14 @@ const app = express();
 
 const PORT = 8888;
 
+var router = require("../Router/Router.js");
+
+app.use("/api", router);
+
 app.get("/", (req, res) => {
     res.send("govind is gay");
 })
+
+
 
 app.listen(PORT, () => console.log("App is listening on port :" + PORT));
