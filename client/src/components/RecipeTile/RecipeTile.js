eeed8346a6f3cea979e08c380d676d65fc9205b8
Daniel <danwei002@gmail.com>
2021-01-16 23:53:26 -0500

    links broke but hopefully its ok now

--- client/src/components/RecipeTile/RecipeTile.js
+++ client/src/components/RecipeTile/RecipeTile.js
@@ -3,45 +3,24 @@ import "./RecipeTile.css"
 
 const apiURL = "http://localhost:8888/"
 
-class RecipeTile extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            isHover: false,
-            recipeData: {}
-        }
-    }
-
-    componentDidMount() {
-        fetch(apiURL + "recipe/recipeInfo/" + this.props.id)
-        .then(response => response.json())
-        .then(data => this.setState(prevState => {
-            return {
-                isHover: prevState.isHover,
-                recipeData: data
-            }
-        }))
-    }
-    
-    handleClick() {
-        fetch(apiURL + "recipe/recipeInfo/" + this.props.id)
+function RecipeTile(props) {
+    function handleClick() {
+        fetch(apiURL + "recipe/recipeInfo/" + props.id)
         .then(response => response.json())
         .then(data =>
             window.open(data.sourceUrl)
         )
     }
 
-    render() {
-        return (
-            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 p-0 mb-4 mt-5">
-                <div role="button" tabIndex={0} className="ml-4 recipe-tile pt-4 pl-3 pr-3">
-                    <h1>{this.props.title}</h1>
-                    <br/>
-                    <img src={this.props.url} className="img-fluid" alt="No"/>
-                </div>
+    return (
+        <div onClick={handleClick} className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 p-0 mb-4 mt-5">
+            <div role="button" tabIndex={0} className="recipe-tile pt-4 pl-3 pr-3">
+                <h1>{props.title}</h1>
+                <br/>
+                <img src={props.url} className="img-fluid"/>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default RecipeTile
