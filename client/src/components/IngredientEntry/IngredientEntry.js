78fd4d3e8c98bd7f4b7ea66ad93e1d53fc7d196a
Daniel <danwei002@gmail.com>
2021-01-16 19:29:05 -0500

    Add autocomplete support to front-end

--- client/src/components/IngredientEntry/IngredientEntry.js
+++ client/src/components/IngredientEntry/IngredientEntry.js
@@ -10,12 +10,15 @@ class IngredientEntry extends React.Component {
         super()
         this.state = {
             ingredients: [],
+            acWords: [],
             apiString: ""
         }
 
         this.addIngredient = this.addIngredient.bind(this)
         this.deleteIngredient = this.deleteIngredient.bind(this)
         this.handleClick = this.handleClick.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.autocompleteHandler = this.autocompleteHandler.bind(this)
     }
     
     addIngredient(e) {
@@ -29,6 +32,7 @@ class IngredientEntry extends React.Component {
             this.setState(prevState => {
                 return {
                     ingredients: prevState.ingredients.concat(newIngredient),
+                    acWords: prevState.acWords,
                     apiString: prevState.apiString
                 }
             })
@@ -41,7 +45,7 @@ class IngredientEntry extends React.Component {
 
     deleteIngredient(key) {
         const remainingIngs = this.state.ingredients.filter(ingredient => ingredient.key !== key)
-        this.setState(prevState => {return {ingredients: remainingIngs, apiString: prevState.apiString}})
+        this.setState(prevState => {return {ingredients: remainingIngs, acWords: prevState.acWords, apiString: prevState.apiString}})
     }
 
     handleClick(e) {
@@ -60,18 +64,46 @@ class IngredientEntry extends React.Component {
         .then(data => console.log(data))
         .catch(Error => { console.log(Error) })
 
-        fetch(apiURL + "recipe/recipeInfo/" + response.)
+        //fetch(apiURL + "recipe/recipeInfo/" + response.)
+    }
+
+    handleChange(e) {
+        e.preventDefault()
+        if (this.ingredientInput.value.length === 0) {return;}
+        const autocompleteQuery = apiURL + "ingredients/autocomplete/" + this.ingredientInput.value
+        const autocomplete = fetch(autocompleteQuery)
+        .then(response => response.json())
+        .then(data => {
+            this.setState(prevState => {
+                return {
+                    ingredients: prevState.ingredients,
+                    acWords: data,
+                    apiString: prevState.apiString
+                }
+            })
+        })
+    }
+
+    autocompleteHandler(e) {
+        e.preventDefault()
+        this.ingredientInput.value = e.target.name
     }
 
     render() {
+        const autocompleteButtons = this.state.acWords.map(acWord =>
+            <button name={acWord} onClick={this.autocompleteHandler} className="btn btn-lg btn-light mr-5 mt-5">{acWord}</button>
+        )
+
         return (
             <div>
                 <form onSubmit={this.addIngredient}>
-                    <input ref={(i) => this.ingredientInput = i} className="ingredient-entry-box mr-3" placeholder="Enter ingredients"/>
+                    <input onChange={this.handleChange} ref={(i) => this.ingredientInput = i} className="ingredient-entry-box mr-3" placeholder="Enter ingredients"/>
                     <button type="submit" className="btn btn-success px-3">Add</button>
                 </form>
                 <IngredientList ingList={this.state.ingredients} delete={this.deleteIngredient}/>
                 <button onClick={this.handleClick} type="submit" className="btn btn-lg btn-warning mt-5">Find Recipes</button>
+                <br/>
+                {autocompleteButtons}
             </div>
         )
     }
